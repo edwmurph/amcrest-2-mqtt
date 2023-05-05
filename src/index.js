f380912c4cec240b73b375242a4aaeea52699eca
edwmurph <20996513+edwmurph@users.noreply.github.com>
2023-05-04 21:38:14 -0400

    publish mqtt events

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 
+const mqtt = require('mqtt');
 const AmcrestEmitter = require('./classes/amcrest-emitter');
+
+const client = mqtt.connect( process.env.MQTT_URL );
 
 const amcrestEmitter = new AmcrestEmitter({
   host: process.env.AMCREST_HOST,
@@ -13,6 +16,10 @@ amcrestEmitter.connect();
 amcrestEmitter.on( 'event', ({ event }) => {
   if ( event.Code === 'PhoneCallDetect' && event.action === 'Start' ) {
     console.log('DOORBELL PRESSED');
+
+    client.publish( 'amcrest-2-mqtt/doorbell1/doorbell-pressed', {
+      date: new Date().toISOString()
+    });
   }
 
   console.log( event );
